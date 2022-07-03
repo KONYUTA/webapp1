7fdaaab3dc6638d79db73b6c33edef7fea17e76f
EC2 Default User <ec2-user@ip-172-31-22-165.ap-northeast-1.compute.internal>
2022-07-03 12:20:39 +0000

    Add menu and tag function

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,20 +1,70 @@
 class MenusController < ApplicationController
     def index
+        @menus = Menu.all
+        @tags = Tag.all
     end
     
     def show
+        @menu = Menu.find(params[:id])
+        @tag_list=@menu.tags.pluck(:name).join(',')
     end
     
     def new
+        @menu = Menu.new
+    end
+    
+    def create
+        @menu = Menu.new(menu_params)
+        @menu.user_id = current_user.id
+        @menu.genre_id = 1
+        if @menu.user != current_user
+            redirect_to user_path(current_user)
+        end
+        tag_list=params[:menu][:tag_name].split(',')
+        if @menu.save
+            tag_list.each do |tag|
+                new_menu_tag = Tag.find_or_create_by(name: tag)
+                @menu.tags << new_menu_tag
+            end
+            redirect_to menu_path(@menu)
+        else
+            render :new
+        end
     end
     
     def edit
+        @menu = Menu.find(params[:id])
+        @tags = @menu.tags.pluck(:name).join(',')
+        if @menu.user != current_user
+            redirect_to user_path(current_user)
+        end
     end
     
     def update
+        @menu = Menu.find(params[:id])
+        @menu.genre_id = 1
+        tag_list=params[:menu][:tag_name].split(',')
+        if @menu.user != current_user
+            redirect_to user_path(current_user)
+        elsif @menu.update(menu_params)
+            @menu.save_tag(tag_list)
+            redirect_to menu_path(@menu)
+        else
+            render :edit
+        end
+        
     end
     
     def destroy
+        @menu = Menu.find(params[:id])
+        if @menu.user != current_user
+            redirect_to user_path(current_user)
+        end
+        if @menu.delete
+            redirect_to user_path(current_user)
+        else
+            redirect_to menu_path(@menu)
+        end
     end
     
     private
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,32 @@
 class Menu < ApplicationRecord
     belongs_to :user
+    belongs_to :genre
+    has_many :items
+    has_many :procedures
+    has_many :menu_tags, dependent: :destroy
+    has_many :tags, through: :menu_tags
+    has_one_attached :menu_image
+    
+    def get_menu_image(size)
+        unless menu_image.attached?
+            file_path = Rails.root.join('app/assets/images/no_image.jpg')
+            menu_image.attach(io: File.open(file_path),filename: 'no-image.jpg', content_type: 'image/jpeg')
+        end
+        menu_image.variant(resize: size).processed
+     end
+     
+     def save_tag(sent_tags)
+        current_tags = self.tags.pluck(:name)
+        old_tags = current_tags - sent_tags
+        new_tags = sent_tags - current_tags
+        
+        old_tags.each do |old|
+            self.tags.delete　Tag.find_by(name: old)
+        end
+        
+        new_tags.each do |new|
+            new_menu_tag = Tag.find_or_create_by(name: new)
+            self.tags << new_menu_tag
+        end
+    end
 end
